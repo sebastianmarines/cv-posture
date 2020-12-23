5779acf3ca0c896bd2fc537a7d661a97990b75e7
Sebastian Marines <sebastian0marines@gmail.com>
2020-12-23 11:21:10 -0600

    Start worker directly

--- main.py
+++ main.py
@@ -18,20 +18,40 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setFixedSize(self.size())
         self.data: tuple = ()
         self.start_mediapipe()
+        self.ui.send_data.clicked.connect(self.send_data)
 
-    def start_mediapipe(self):
-        self.thread = QThread()
+    def start_mediapipe(self) -> None:
+        # self.mp_thread = QThread()
         # Step 3: Create a mpworker object
         self.mpworker = MPThread(debug=settings.DEBUG)
         # Step 4: Move mpworker to the thread
-        self.mpworker.moveToThread(self.thread)
+        # self.mpworker.moveToThread(self.mp_thread)
         # Step 5: Connect signals and slots
         # noinspection PyUnresolvedReferences
-        self.thread.started.connect(self.mpworker.run)
+        # self.mp_thread.started.connect(self.mpworker.run)
         self.mpworker.new_frame.connect(self.show_image)
         self.mpworker.data.connect(self.store_data)
         # Step 6: Start the thread
-        self.thread.start()
+        self.mpworker.start()
+
+    def send_data(self) -> None:
+        print(self.data)
+        # Step 2: Create a QThread object
+        self.data_thread = QThread()
+        # Step 3: Create a worker object
+        self.data_worker = DataSave(self.data)
+        # # Step 4: Move worker to the thread
+        # self.data_worker.moveToThread(self.data_thread)
+        # # Step 5: Connect signals and slots
+        # # noinspection PyUnresolvedReferences
+        # self.data_thread.started.connect(self.data_worker.run)
+        # # Step 6: Start the thread
+        # self.data_thread.start()
+        #
+        # self.ui.send_data.setEnabled(False)
+        # self.data_thread.finished.connect(
+        #     lambda: self.ui.send_data.setEnabled(True)
+        # )
 
     @QtCore.pyqtSlot(QImage)
     def show_image(self, image):
