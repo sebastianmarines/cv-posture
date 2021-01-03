e5dc7f336be847e800ff91e803ab4d1ba0656f3e
Sebastian Marines <sebastian0marines@gmail.com>
2021-01-02 19:05:14 -0600

    Add return type annotation to cv_to_qimage function

--- utils/images.py
+++ utils/images.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage
 
 
-def cv2_to_qimage(img: object) -> object:
+def cv2_to_qimage(img) -> QImage:
     h, w, ch = img.shape
     bytes_per_line = ch * w
     convert_to_qt_format = QImage(img.data, w, h, bytes_per_line, QImage.Format_RGB888)
